855aa900a11d0f9a521d4b809aab9a5411500f23
FIN-THE-HUMAN <malyshevi@yandex.ru>
2016-11-29 18:17:35 +0300

    Update QuizVariantModel.java

--- quiz/src/main/java/com/github/sasd97/models/QuizVariantModel.java
+++ quiz/src/main/java/com/github/sasd97/models/QuizVariantModel.java
@@ -1,15 +1,43 @@
 package com.github.sasd97.models;
 
-/**
- * Created by Alexadner Dadukin on 11/29/2016.
- */
+import com.google.gson.annotations.Expose;
 
-public class QuizVariantModel {
+import javax.persistence.*;
+import java.time.LocalDateTime;
 
-    public enum Type {
-        TEXT,
-        IMAGE
+@Entity
+@Table(name = "answers")
+public class AnswerModel
+{
+    @Expose
+    private int type;
+
+    @Expose
+    private String value;
+
+    public int getType()
+    {
+        return type;
     }
 
+    public String getValue()
+    {
+        return value;
+    }
 
+    public void setType(int type) {
+        this.type = type;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return "AnswerModel{" +
+                ", type='" + type + '\'' +
+                ", value='" + value + '\''  +
+                '}';
+    }
 }
