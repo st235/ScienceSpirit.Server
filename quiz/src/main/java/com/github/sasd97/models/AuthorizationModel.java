7588e1a17586076ad06ea1b71bccbdaa6a5d680b
FIN-THE-HUMAN <malyshevi@yandex.ru>
2016-11-29 19:38:53 +0300

    Update AuthorizationModel.java

--- quiz/src/main/java/com/github/sasd97/models/AuthorizationModel.java
+++ quiz/src/main/java/com/github/sasd97/models/AuthorizationModel.java
@@ -3,16 +3,45 @@ package com.github.sasd97.models;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
-/**
- * Created by Alexadner Dadukin on 11/29/2016.
- */
-
 @Entity
 @Table(name = "authorization")
 public class AuthorizationModel {
 
-    //TODO: сделать пару
-    //creation date - дата создания
-    //userId - идешник юзера
-    //token - токен (строка)
+    @Expose
+    @Column(nullable = false)
+    private LocalDateTime creationDate = LocalDateTime.now();
+
+    @Expose
+    private String token;
+
+    @Id
+    @Expose
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long userId;
+
+    public LocalDateTime getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(LocalDateTime creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
 }
+
