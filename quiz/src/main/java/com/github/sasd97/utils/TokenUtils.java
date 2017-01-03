3aeb343129ab4d984b6fbf622f59a2cc8423ea9a
Alexadner Dadukin <st235@yandex.ru>
2017-01-03 20:36:22 +0300

    Added and tested token expires system

--- quiz/src/main/java/com/github/sasd97/utils/TokenUtils.java
+++ quiz/src/main/java/com/github/sasd97/utils/TokenUtils.java
@@ -26,25 +26,32 @@ public class TokenUtils {
     public AuthorizationModel getToken(@NotNull String token) {
         List<AuthorizationModel> authorizations = authorizationRepository.findByToken(token);
         if (authorizations.size() == 0) throw new NotAuthorizedError();
-        return authorizations.get(0);
+        AuthorizationModel auth = authorizations.get(0);
+        if (!holdAuth(auth)) throw new NotAuthorizedError();
+        return auth;
     }
 
 
     public boolean withToken(@NotNull String token, @NotNull Long id) {
         List<AuthorizationModel> authorizations = authorizationRepository.findByToken(token);
         if (authorizations.size() == 0) return false;
+        if (!holdAuth(authorizations.get(0))) return false;
         if (!authorizations.get(0).getUserId().equals(id)) return false;
         return true;
     }
 
     public boolean isToken(@NotNull String token) {
         List<AuthorizationModel> authorizations = authorizationRepository.findByToken(token);
-        return authorizations.size() != 0;
+        if (authorizations.size() == 0) return false;
+        if (!holdAuth(authorizations.get(0))) return false;
+        return true;
     }
 
     public boolean isAdminToken(@NotNull String token) {
-        List<AuthorizationModel> authorization  = authorizationRepository.findAdminsByToken(token);
-        return !(authorization == null || authorization.size() > 1 || authorization.size() == 0);
+        List<AuthorizationModel> authorization = authorizationRepository.findAdminsByToken(token);
+        if (authorization.size() == 0) return false;
+        if (!holdAuth(authorization.get(0))) return false;
+        return true;
     }
 
     public String trimSocketToken(@NotNull String socketToken) {
